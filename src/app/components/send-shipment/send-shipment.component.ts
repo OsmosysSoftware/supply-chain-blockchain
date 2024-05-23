ca93682d5e043af5a5028823ed4b62bd7bf6c002
xixas <vikaskyatannawar@gmail.com>
2024-05-23 10:58:32 +0530

    Update src/app/components/send-shipment/send-shipment.component.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/app/components/send-shipment/send-shipment.component.ts
+++ src/app/components/send-shipment/send-shipment.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-send-shipment',
   templateUrl: './send-shipment.component.html',
-  styleUrl: './send-shipment.component.scss',
+  styleUrls: ['./send-shipment.component.scss'],
 })
 export class SendShipmentComponent {}
